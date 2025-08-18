394acb05baa7938dbc5b3bd330b74b4d1f6fb66e
S-M-Mazharul-Islam-RIfat <smmjrifat@gmail.com>
2025-08-18 11:26:04 +0600

    added round 1042 codes

--- Codechef Starters 191/Add_1_or_2.cpp
+++ Codechef Starters 191/Add_1_or_2.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-bool isPossible(ll mid, vector<ll>&a, vector<ll>&b, ll n){
-    vector<ll>ache;//
-    ll event=0;
-    for(int i=0; i<n; i++){
-        ll gap = mid-a[i];//3
-        if(gap>=b[i]){
-            ache.push_back(gap-b[i]);
+bool isPossible(ll mid, vector<ll>& a, vector<ll>& b, ll n) {
+    vector<ll>ache;
+    ll event = 0;
+    for (int i = 0; i < n; i++) {
+        ll gap = mid - a[i];
+        if (gap >= b[i]) {
+            ache.push_back(gap - b[i]);
         }
-        else{
+        else {
             event += b[i] - gap;
         }
     }
 
-    for(ll val : ache){
-        event -= val/2;
+    for (ll val : ache) {
+        event -= val / 2;
     }
 
-    if(event<=0) return true;
+    if (event <= 0) return true;
     else return false;
 
 }
@@ -28,33 +28,33 @@ int main() {
     cin.tie(NULL);
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         ll n;
         cin >> n;
 
         vector<ll>a(n);
         vector<ll>b(n);
-        ll mx=0;
-        for(ll i=0; i<n; i++){
+        ll mx = 0;
+        for (ll i = 0; i < n; i++) {
             cin >> a[i];
             mx = max(mx, a[i]);
         }
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             cin >> b[i];
         }
 
-        ll low=mx, high=1e14, ans=high;
+        ll low = mx, high = 1e14, ans = high;
 
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(isPossible(mid, a, b, n)){
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isPossible(mid, a, b, n)) {
                 ans = mid;
-                high = mid-1;
+                high = mid - 1;
             }
-            else low = mid+1;
+            else low = mid + 1;
         }
 
-        cout << ans  << '\n';
+        cout << ans << '\n';
 
     }
 }
